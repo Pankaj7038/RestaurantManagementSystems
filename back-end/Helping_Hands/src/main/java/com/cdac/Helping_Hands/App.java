7cfdc3a305a12a0203597d976f3e6b401f39af17
Pankaj7038 <pankajpatil03121999@gmail.com>
2023-09-11 19:48:42 +0530

    Add frontend-backend file uploaded

---
+++ back-end/Helping_Hands/src/main/java/com/cdac/Helping_Hands/App.java
@@ -0,0 +1,13 @@
+package com.cdac.Helping_Hands;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
